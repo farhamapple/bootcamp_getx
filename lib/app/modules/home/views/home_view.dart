13098ac937a2a0d76b393294ce5fd70eab50f170
farhamapple@gmail.com <farhamapple@gmail.com>
2021-12-05 09:53:25 +0700

    Dialog

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -9,13 +9,26 @@ class HomeView extends GetView<HomeController> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('HomeView'),
+        title: const Text('HomeView'),
         centerTitle: true,
       ),
       body: Center(
-        child: Text(
-          'HomeView is working',
-          style: TextStyle(fontSize: 20),
+        child: ElevatedButton(
+          onPressed: () {
+            //Get.dialog(const Text('data'));
+            Get.defaultDialog(
+              title: "Test",
+              middleText: "Peass",
+              barrierDismissible: true,
+              backgroundColor: Colors.amber,
+            );
+          },
+          child: const Text(
+            'Button',
+            style: TextStyle(
+              fontSize: 20,
+            ),
+          ),
         ),
       ),
     );
